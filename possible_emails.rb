0ea601eee9bc9eb4c2d1839c69a5f02d72bec0de
Kristen MacFarlane <krismacfarlane@gmail.com>
2015-03-04 10:58:32 -0500

    initial commit

---
+++ possible_emails.rb
@@ -0,0 +1,43 @@
+require 'pry'
+
+class PossibleEmails
+
+  attr_accessor :fname, :lname, :domain, :name
+
+  def initialize(name, domain)
+    @name = name.downcase
+    @fname = name.downcase.split(" ")[0]
+    @lname = name.downcase.split(" ")[1]
+    @domain = "@" + domain
+  end
+
+  def first_name_dot_last_name
+    @fname + "." + @lname + @domain
+  end
+
+  def first_name_dot_last_initial
+    @fname + "." + @lname[0] + @domain
+  end
+
+  def first_initial_dot_last_name
+    @fname[0] + "." + @lname + @domain
+  end
+
+  def first_initial_dot_last_initial
+    @fname[0] + "." + @lname[0] + @domain
+  end
+
+  def predict
+    if domain == "@ironthrone.com"
+      puts "The possible email address for #{name} is #{first_name_dot_last_name}."
+    elsif domain == "@direwolves.com"
+      puts "The possible email addresses for #{name} are #{first_name_dot_last_initial} or #{first_initial_dot_last_name}."
+    elsif domain == "@littefinger.com"
+      puts "The possible email address for #{name} is #{first_initial_dot_last_initial}."
+    else
+      puts "The possible email addresses for #{name} are #{first_name_dot_last_name}, #{first_name_dot_last_initial}, #{first_initial_dot_last_initial}, or #{first_initial_dot_last_name}."
+    end
+  end
+
+end
+
---
+++ predict_these.rb
@@ -0,0 +1,13 @@
+require 'pry'
+require_relative 'possible_emails.rb'
+
+margaery = PossibleEmails.new("Margaery Tyrell", "ironthrone.com")
+theon = PossibleEmails.new("Theon Greyjoy", "direwolves.com")
+alayne = PossibleEmails.new("Alayne Stone", "littlefinger.com")
+jack = PossibleEmails.new("Jack Qyburn", "citadel.gov")
+
+margaery.predict
+theon.predict
+alayne.predict
+jack.predict
+
